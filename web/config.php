172f756c6c2426c5854894aad4163198e0f6b3b4
tdapc <35833046+tdapc@users.noreply.github.com>
2018-01-26 20:15:05 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyDnZai19gPR0RS-f1OISNWlpNjo17lwbdA');//这里改成您的api key
+define('APIKEY', 'AIzaSyDnZai1R0RS-f1OISNWlpNjo17lwbdA');//这里改成您的api key
 define('GJ_CODE', 'TW');//国家代码
 define('SITE_NAME', 'feizhuge');//网站名字
 define('TITLENAME', 'tdapc');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', 'tdapc1@gmail.com');//邮箱
+define('EMAIL', 'td1@gmail.com');//邮箱
 ?>
